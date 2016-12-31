76adb5e2955d7f872d101a21229b90f6bb82440e
Russell Jennings <violentpurr@gmail.com>
2016-12-31 01:35:39 -0500

    misc fixes

--- lib/middleman-dev-cms.rb
+++ lib/middleman-dev-cms.rb
@@ -3,9 +3,9 @@
 require 'middleman-core'
 
 ::Middleman::Extensions.register(:dev_cms) do
-  module Middleman::Blog::Ui
+  module Middleman::Dev::Cms
     SOURCE_DIR ||= File.expand_path(File.join('..', '..', 'source'), __FILE__)
   end
   require 'middleman/dev/cms/extension'
-  ::Middleman::Blog::Ui::Extension
+  ::Middleman::Dev::Cms::Extension
 end
